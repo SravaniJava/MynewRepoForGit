187edaaa4eeeffcefccd4171852aa44eb964640a
sravaniPal <sravanigoudjava.com>
2025-03-23 17:45:58 +0530

    new class added by master i/e newmaster

--- src/main/java/org/palni/sravniChildSub.java
+++ src/main/java/org/palni/sravniChildSub.java
@@ -4,5 +4,6 @@ public class sravniChildSub {
     public static void sub(){
         System.out.println("this is sub from the sravani child");
         System.out.println("this is changes in child1 ");
+        System.out.println("this is from child1");
     }
 }

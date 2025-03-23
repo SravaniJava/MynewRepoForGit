b17355a5d52fd169308f13da2e5b84bee5476deb
sravaniPal <sravanigoudjava.com>
2025-03-23 12:47:47 +0530

    changes added in childsub

--- src/main/java/org/palni/sravniChildSub.java
+++ src/main/java/org/palni/sravniChildSub.java
@@ -3,5 +3,6 @@ package org.palni;
 public class sravniChildSub {
     public static void sub(){
         System.out.println("this is sub from the sravani child");
+        System.out.println("this is changes in child1 ");
     }
 }

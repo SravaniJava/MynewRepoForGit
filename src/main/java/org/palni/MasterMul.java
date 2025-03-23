3f54e43b7c9dbde79f8cfbfa869826597b7de2f3
sravaniPal <sravanigoudjava.com>
2025-03-23 13:35:54 +0530

    changes added in Master Mul by sravanichild

--- src/main/java/org/palni/MasterMul.java
+++ src/main/java/org/palni/MasterMul.java
@@ -5,5 +5,6 @@ public class MasterMul {
         System.out.println("this is master mul");
         System.out.println("this is changes added from sravani child class in master mul");
         System.out.println("this is changes added from the master mul for the second time in the master mul ");
+        System.out.println("mastermull changes from master at 1:35 without pull");
     }
 }

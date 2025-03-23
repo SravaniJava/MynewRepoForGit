1321ca02b5a7b8942d93803d55fe2880133f8685
sravaniPal <sravanigoudjava.com>
2025-03-23 14:42:51 +0530

    mastermethod  added in MasterMul  in Master Mul by  master

--- src/main/java/org/palni/MasterMul.java
+++ src/main/java/org/palni/MasterMul.java
@@ -17,6 +17,12 @@ public class MasterMul {
     }
 
 
+
+    public static void masterMethod(){
+        System.out.println("from master mul by master at 2:245");
+    }
+
+
     public static void main(String[] args) {
         System.out.println("this is master mul");
         System.out.println("this is changes added from sravani child class in master mul");

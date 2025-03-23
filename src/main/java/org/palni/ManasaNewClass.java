eaebbcd9278ffb2708d381d60b8569b223e748c1
sravaniPal <sravanigoudjava.com>
2025-03-23 17:51:13 +0530

    new class added by master i/e newmaster

--- src/main/java/org/palni/ManasaNewClass.java
+++ src/main/java/org/palni/ManasaNewClass.java
@@ -5,5 +5,6 @@ public class ManasaNewClass {
         System.out.println("this is new java class");
         System.out.println("this is my new class method of main ");
         System.out.println("jsut checking from the mansa in mansanewclass");
+        System.out.println("new at5:50");
     }
 }

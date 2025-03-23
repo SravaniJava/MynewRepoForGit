a75e15e8a9aba498508adcd0f5d3b3f096daeecb
sravaniPal <sravanigoudjava.com>
2025-03-23 14:30:56 +0530

    manasadiv method in mansa method  in Master Mul by  mansa

--- src/main/java/org/palni/MasterMul.java
+++ src/main/java/org/palni/MasterMul.java
@@ -11,6 +11,12 @@ public class MasterMul {
         System.out.println("this is from mastermul by master at 2:15");
 
     }
+
+    public static void manasadiv(){
+        System.out.println("this is the div method added in master mul by manasa");
+    }
+
+
     public static void main(String[] args) {
         System.out.println("this is master mul");
         System.out.println("this is changes added from sravani child class in master mul");

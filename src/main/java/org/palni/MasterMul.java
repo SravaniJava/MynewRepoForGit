97a9eb46390e79b194d84301867e9a4206d98b15
sravaniPal <sravanigoudjava.com>
2025-03-23 13:00:50 +0530

    changes added in childsub

--- src/main/java/org/palni/MasterMul.java
+++ src/main/java/org/palni/MasterMul.java
@@ -3,5 +3,6 @@ package org.palni;
 public class MasterMul {
     public static void main(String[] args) {
         System.out.println("this is master mul");
+        System.out.println("this is changes added from sravani child class in master mul");
     }
 }

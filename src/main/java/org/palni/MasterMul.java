737a55e587403017650c821e1b6fa68d7ab368b5
sravaniPal <sravanigoudjava.com>
2025-03-23 13:14:15 +0530

    changes added in Master Mul by Master branch

--- src/main/java/org/palni/MasterMul.java
+++ src/main/java/org/palni/MasterMul.java
@@ -4,5 +4,6 @@ public class MasterMul {
     public static void main(String[] args) {
         System.out.println("this is master mul");
         System.out.println("this is changes added from sravani child class in master mul");
+        System.out.println("this is changes added from the master mul for the second time in the master mul ");
     }
 }
